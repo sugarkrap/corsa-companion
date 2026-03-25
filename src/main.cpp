37355bfb66b7e8bd97a7cd263403c721213ce154
Makaron <makaron@pwet.pl>
2026-03-25 16:06:22 +0100

    change for arduino nano

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,7 @@ void setup() {
 
     Serial.println("# SWC voltage calibration");
     Serial.println("# Serial commands: ON / OFF");
-<<<<<<< HEAD
     Serial.println("# Plotter columns: Voltage_V");
-=======
-    Serial.println("# Button A: toggle sleep mode");
->>>>>>> f9cd1c4 (oled and buttons)
 }
 
 void loop() {
